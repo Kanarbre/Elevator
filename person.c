ed3f5b872f7f6e4f75311dbe39d507268673106e
Kanarbre <zelou4@gmail.com>
2020-09-09 00:48:09 +0200

    person.c & elevator.c added

---
+++ person.c
@@ -0,0 +1,15 @@
+# include "person.h"
+
+Person* createPerson(int src,int dest){
+    Person* t = (Person*)malloc(sizeof(Person));
+    t -> src = src;
+    t -> dest = dest;
+    return t;
+}
+
+PersonList* insert(Person *p, PersonList *list){
+    PersonList *t = (PersonList*)malloc(sizeof(PersonList));
+    t -> person = p;
+    t -> next = list;
+    return t;
+}
